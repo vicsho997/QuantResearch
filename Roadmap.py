ae0c75fcf652a45c7f644376d36d846c1cc6de50
vicsho997 <54086674+vicsho997@users.noreply.github.com>
2020-04-15 13:58:55 -0500

    Add files via upload
    First look at quant before computer died

---
+++ Roadmap.py
@@ -0,0 +1,37 @@
+#Chapter 2 looks at the overall topology of the ppf package, its dependencies and how to build, install and test it (newcomers to Python may be served by looking ahead to Appendix A where a quick tutorial on Python basics is offered)
+
+#Chapter 3 considers the topic of implementing Python extension modules in C++ with an emphasisonfosteringinteroperabilitywithexistingC++ﬁnancialengineeringsystemsand,in particular,howcertainfunctionalitypresentinppfinfactisunderliedbyC++inthisfashion. 
+
+#Chapter 4 lays the groundwork for later chapters (concerned with pricing using techniques from numerical analysis) in that it presents those mathematical algorithms and tools that arise over and over again in computational quantitative analysis, including:
+        #1) (pseudo) random number generation
+        #2) estimation of the standard normal cumulative distribution function;
+        #3) a variety of interpolation schemes
+        #4) root-ﬁnding algorithms
+        #5) various operations for linear algebra
+        #6) generalised linear least-squares data ﬁtting
+        #7) stable calculation techniques for computing quadratic and cubic roots; and 
+        #8) calculation of the expectation of a function of a random variable.
+        
+#Chapter 5 looks at how the ppf represents common market information such as discountfactor functions and volatility surfaces.
+        
+#Chapter 6 is entirely concerned with looking at the data structures used in the ppf for representingﬁnancialstructures:‘ﬂows’,‘legs’,‘exerciseopportunities’,‘trades’andthelike. 
+        
+#Chapter 7 details the concepts and classes that govern the interactions between the trade representations and pricing models in the ppf package. 
+        
+#Chapter 8 offers an implementation of a fully functional Hull–White model in Python, where the characteristic features of the model are assembled from (in as much as is possible) functionally orthogonal components. 
+        
+#Chapter9 present two general numerical pricing frameworks invariant over pricing models: one lattice based, the other Monte-Carlo based.
+
+#Chapter 10 applies the pricing frameworks and the Hull–White model developed in the preceding chapters to pricing ﬁnancial structures,speciﬁcally,Bermudan swaptions and target redemption note
+        
+#Chapter 11, while keeping things tractable, introduces the idea of and practical techniques for C++/Python ‘Hybrid Systems’ against the backdrop of existing derivative security pricing and risk management systems in C++. 
+        
+#Chapter 12 gives concrete examples of implementing COM servers in Python and utilising the functionality so exposed in the context of Microsoft Excel
+        
+#https://onlinelibrary.wiley.com/doi/pdf/10.1002/9780470685006.app2
+#https://onlinelibrary.wiley.com/doi/book/10.1002/9780470685006
+        
+        
+        
+        
+        
---
+++ RoadmapFirstEquation.py
@@ -0,0 +1,22 @@
+#nonlinear
+#f(t) = (f(xx))
+
+#copula model and economic cycles
+
+#copula model
+
+#nonlinear finite element analysis
+
+#paul wilmott derivatives
+"""
+https://forum.wilmott.com/viewforum.php?f=38&sid=24f28678984be61d125fb05ab389cc8a
+https://forum.wilmott.com/viewforum.php?f=4&sid=24f28678984be61d125fb05ab389cc8a
+https://forum.wilmott.com/viewforum.php?f=34&sid=24f28678984be61d125fb05ab389cc8a
+https://forum.wilmott.com/viewforum.php?f=41&sid=24f28678984be61d125fb05ab389cc8a
+https://forum.wilmott.com/viewforum.php?f=8&sid=24f28678984be61d125fb05ab389cc8a
+
+https://forum.wilmott.com/viewforum.php?f=44&sid=24f28678984be61d125fb05ab389cc8a
+https://github.com/cantaro86/Financial-Models-Numerical-Methods
+
+
+"""
